7e90665cfd5e219742d915920039769e04fa0be0
AndrewK <andrew.krivosheyenko@gmail.com>
2025-12-09 11:23:23 +0200

    added: DI for Spectre Command

--- src/FTPSheep.CLI/Program.cs
+++ src/FTPSheep.CLI/Program.cs
@@ -1,59 +1,97 @@
+using FTPSheep.CLI;
 using FTPSheep.CLI.Commands;
+using Microsoft.Extensions.DependencyInjection;
 using NLog;
+using NLog.Extensions.Logging;
 using Spectre.Console.Cli;
 
-namespace FTPSheep.CLI;
+var logger = LogManager.GetCurrentClassLogger();
 
-internal class Program {
-    private static int Main(string[] args) {
-        var logger = LogManager.GetCurrentClassLogger();
+try {
+    logger.Info("FTPSheep.NET starting up...");
 
-        try {
-            logger.Info("FTPSheep.NET starting up...");
+    var registrations = new ServiceCollection();
+    registrations.AddNLog();
 
-            var app = new CommandApp();
+    var app = new CommandApp(new TypeRegistrar(registrations));
 
-            app.Configure(config => {
-                config.SetApplicationName("ftpsheep");
-                config.SetApplicationVersion("0.1.0");
+    app.Configure(config => {
+        config.SetApplicationName("ftpsheep");
+        config.SetApplicationVersion("0.1.0");
 
-                config.AddCommand<DeployCommand>("deploy")
-                    .WithDescription("Deploy a .NET application to FTP server")
-                    .WithExample("deploy", "--profile", "production")
-                    .WithExample("deploy", "--yes");
+        config.AddCommand<DeployCommand>("deploy")
+            .WithDescription("Deploy a .NET application to FTP server")
+            .WithExample("deploy", "--profile", "production")
+            .WithExample("deploy", "--yes");
 
-                config.AddBranch("profile", profile => {
-                    profile.SetDescription("Manage deployment profiles");
+        config.AddBranch("profile", profile => {
+            profile.SetDescription("Manage deployment profiles");
 
-                    profile.AddCommand<ProfileListCommand>("list")
-                        .WithDescription("List all deployment profiles");
+            profile.AddCommand<ProfileListCommand>("list")
+                .WithDescription("List all deployment profiles");
 
-                    profile.AddCommand<ProfileCreateCommand>("create")
-                        .WithDescription("Create a new deployment profile");
-                });
+            profile.AddCommand<ProfileCreateCommand>("create")
+                .WithDescription("Create a new deployment profile");
+        });
 
-                config.AddCommand<ImportCommand>("import")
-                    .WithDescription("Import Visual Studio publish profile")
-                    .WithExample("import", "Properties/PublishProfiles/FTP.pubxml");
+        config.AddCommand<ImportCommand>("import")
+            .WithDescription("Import Visual Studio publish profile")
+            .WithExample("import", "Properties/PublishProfiles/FTP.pubxml");
 
-                config.AddCommand<InitCommand>("init")
-                    .WithDescription("Initialize a new deployment profile interactively");
+        config.AddCommand<InitCommand>("init")
+            .WithDescription("Initialize a new deployment profile interactively");
 
 #if DEBUG
-                config.PropagateExceptions();
-                config.ValidateExamples();
+        config.PropagateExceptions();
+        config.ValidateExamples();
 #endif
-            });
+    });
 
-            var result = app.Run(args);
+    var result = app.Run(args);
 
-            logger.Info("FTPSheep.NET shutting down with exit code {ExitCode}", result);
-            return result;
-        } catch(Exception ex) {
-            logger.Fatal(ex, "Stopped program because of exception");
-            return 1;
-        } finally {
-            LogManager.Shutdown();
+    logger.Info("FTPSheep.NET shutting down with exit code {ExitCode}", result);
+    return result;
+} catch(Exception ex) {
+    logger.Fatal(ex, "Stopped program because of exception");
+    return 1;
+} finally {
+    LogManager.Shutdown();
+}
+
+namespace FTPSheep.CLI {
+    file class TypeRegistrar(IServiceCollection builder) : ITypeRegistrar {
+        public ITypeResolver Build() {
+            return new TypeResolver(builder.BuildServiceProvider());
+        }
+
+        public void Register(Type service, Type implementation) {
+            builder.AddSingleton(service, implementation);
+        }
+
+        public void RegisterInstance(Type service, object implementation) {
+            builder.AddSingleton(service, implementation);
+        }
+
+        public void RegisterLazy(Type service, Func<object> func) {
+            if(func is null) {
+                throw new ArgumentNullException(nameof(func));
+            }
+
+            builder.AddSingleton(service, (provider) => func());
+        }
+    }
+
+    file sealed class TypeResolver(IServiceProvider provider) : ITypeResolver, IDisposable {
+        private readonly IServiceProvider provider = provider ?? throw new ArgumentNullException(nameof(provider));
+
+        public object? Resolve(Type? type) {
+            return type == null ? null : provider.GetService(type);
+        }
+
+        public void Dispose() {
+            if(provider is IDisposable disposable) {
+                disposable.Dispose();
+            }
         }
     }
 }
