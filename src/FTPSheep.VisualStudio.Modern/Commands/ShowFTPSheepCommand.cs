77c26d752e5ef4949336f5b3417d7f47635334e2
AndrewK <andrew.krivosheyenko@gmail.com>
2026-01-03 21:57:44 +0200

    added keyboard shortcut to the publish window

--- src/FTPSheep.VisualStudio.Modern/Commands/ShowFTPSheepCommand.cs
+++ src/FTPSheep.VisualStudio.Modern/Commands/ShowFTPSheepCommand.cs
@@ -1,6 +1,6 @@
+using FTPSheep.VisualStudio.Modern.ToolWindows;
 using Microsoft.VisualStudio.Extensibility;
 using Microsoft.VisualStudio.Extensibility.Commands;
-using FTPSheep.VisualStudio.Modern.ToolWindows;
 
 namespace FTPSheep.VisualStudio.Modern.Commands;
 
@@ -9,28 +9,26 @@ namespace FTPSheep.VisualStudio.Modern.Commands;
 /// This command appears in the View > Other Windows menu.
 /// </summary>
 [VisualStudioContribution]
-public class ShowFTPSheepCommand : Command
-{
-    public ShowFTPSheepCommand(VisualStudioExtensibility extensibility)
-        : base(extensibility)
-    {
+public class ShowFTPSheepCommand : Command {
+    /// <inheritdoc />
+    public ShowFTPSheepCommand(VisualStudioExtensibility extensibility) : base(extensibility) {
     }
 
     /// <summary>
     /// Command configuration - appears in View > Other Windows menu.
     /// </summary>
-    public override CommandConfiguration CommandConfiguration => new("Publish with FTPSheep")
-    {
-        Placements = new[] { CommandPlacement.KnownPlacements.ToolsMenu },
-        Icon = new(ImageMoniker.KnownValues.ToolWindow, IconSettings.IconAndText),
-    };
+    public override CommandConfiguration CommandConfiguration =>
+        new("Publish with FTPSheep") {
+            Placements = [CommandPlacement.KnownPlacements.ToolsMenu],
+            Icon = new(ImageMoniker.KnownValues.ToolWindow, IconSettings.IconAndText),
+            Shortcuts = [new CommandShortcutConfiguration(ModifierKey.ControlShiftLeftAlt, Key.P)],
+        };
 
     /// <summary>
     /// Execute the command - show the FTPSheep tool window.
     /// </summary>
-    public override async Task ExecuteCommandAsync(IClientContext context, CancellationToken cancellationToken)
-    {
+    public override async Task ExecuteCommandAsync(IClientContext context, CancellationToken cancellationToken) {
         // Show the tool window
-        await this.Extensibility.Shell().ShowToolWindowAsync<FTPSheepToolWindow>(activate: true, cancellationToken);
+        await Extensibility.Shell().ShowToolWindowAsync<FTPSheepToolWindow>(activate: true, cancellationToken);
     }
 }
